43c8b5d061317dad2dc8f3bef5703ad3285a9d32
Limchasu <mayblue20@naver.com>
2024-04-19 18:31:23 +0900

    이제 됩니다 수고하셨습니다.

--- mini.project/src/ex_2/limchaesu.java
+++ mini.project/src/ex_2/limchaesu.java
@@ -1,5 +1,26 @@
 package ex_2;
 
-public class limchaesu {
+import java.util.Scanner;
 
+public class limchaesu {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        int firstNumber = scanner.nextInt();
+
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        int secondNumber = scanner.nextInt();
+
+        int sum = firstNumber + secondNumber;
+        System.out.println("두 숫자의 합: " + sum);
+
+        scanner.close();
+    }
 }
+
+
+
+
+
+
